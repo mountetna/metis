217f3d68a654dc48765a1fca598cf8c88d5b0863
Zachary Collins <recursive.cookie.jar@gmail.com>
2020-06-08 09:14:33 -0700

    Fixing implicit GEM_PATH issue for gems installation

--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 : "${RANDOM_MAX:=500}"
 [ -n "$DEBUG" ] && echo "Running: $@"
-export PATH="/app/vendor/bundle/$RUBY_VERSION/bin:$PATH"
+export PATH="/app/node_modules/.bin:/app/vendor/bundle/$RUBY_VERSION/bin:$PATH"
 
 if [ -z "$SKIP_RUBY_SETUP" ]; then
   bundle check || bundle install -j "$(nproc)"
